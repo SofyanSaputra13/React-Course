8fa4bb9baee24e80cc0d91387d940f2e6fb37352
SofyanSaputra13 <sofyan.saputra@students.ac.id>
2022-09-07 15:45:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const page = (
+  <div>
+    <img src="/image/react-logo.png" width="40px" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Was well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(page, document.getElementById("root"));
+
+//Declarative & Imperative difference
+//We could tell the program/computer what to doand it would know how to do the specific step by step details where imperative we still need to tell the computer step by step how to do everything in order to accompolished something
